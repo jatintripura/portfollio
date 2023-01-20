94675041d925104019a7d7931a084c149ff63849
jatintripura <jatintripura49@gmail.com>
2023-01-20 21:13:09 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+window.onload = function () {
+  const preloaderArea = document.getElementById("preloader-area");
+  preloaderArea.className = "page-loaded";
+  setTimeout(function () {
+    preloaderArea.style.display = "none";
+  }, 1000);
+};
+
+const submitButton = document.getElementById("submit-button");
+
+
+submitButton.addEventListener("click", function () {
+  const nameInput = document.getElementById("full-name");
+  const emailInput = document.getElementById("email-adress");
+  const messageInput = document.getElementById("message");
+  const erorText = document.getElementById("error-text");
+  const submited= document.getElementById("submited")
+
+  const nameValue = nameInput.value;
+  const emailValue = emailInput.value;
+  const messageValue = messageInput.value;
+
+
+  const templateParams = {
+    nameValue: nameValue,
+    emailValue: emailValue,
+    messageValue: messageValue,
+  };
+
+  if (nameValue && emailValue && messageValue) {
+    console.log(nameValue, emailValue, messageValue);
+
+    erorText.innerHTML = ""; 
+    nameInput.value = ""; 
+    emailInput.value = "";
+    messageInput.value="";
+    submited.innerHTML='<p> Thank you!  You have submitted the form. </p>';
+    console.log(submited)
+
+    emailjs.send(
+      '1234',
+      '123456',
+      templateParams,
+      'bElYdbXhucqHpP-0S'
+    
+    );
+  } else {
+    submited.innerHTML=""
+    erorText.innerHTML ='<p class="text-danger">Please! Fill the form and submit. </p>';
+  }
+
+  console.log(nameValue, emailValue, messageValue);
+});
